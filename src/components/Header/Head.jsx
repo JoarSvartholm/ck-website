bf09341faa6f2a967f3866a8246c7f2042ecd943
Joar <joar.svartholm@gmail.com>
2017-09-29 21:57:51 +0200

    Improved the header for all devices

--- src/components/Header/Head.jsx
+++ src/components/Header/Head.jsx
@@ -6,9 +6,12 @@ export class Head extends React.Component{
   render(){
     return (
       <div className="head">
-        <img src={require('./img/ck-logo.png')} alt={'ck fysio'} height="100%" />
+        <img className="head-logo" src={require('./img/ck-logo.png')} alt={'ck fysio'} height="100%" />
         <Link className="fb" to="https://www.facebook.com/cecilia.kalla">
-          <img src={require('./img/fb-logo.png')} alt={'Facebook'} height="40%" />
+          <img src={require('./img/fb-logo.png')} alt={'Facebook'} height="100%" />
+        </Link>
+        <Link className="ig" to="https://www.instagram.com/ceciliakalla/">
+          <img src={require('./img/ig-logo.png')} alt={'Instagram'} height="100%" />
         </Link>
       </div>
     )
--- src/components/Header/Navbar.jsx
+++ src/components/Header/Navbar.jsx
@@ -5,34 +5,10 @@ import { slide as Menu } from 'react-burger-menu'
 import './Header.css'
 
 export class Navbar extends React.Component{
-  constructor(){
-    super();
-    this.handleWindowChange = this.handleWindowChange.bind(this);
-    this.state = {
-      width: window.innerWidth,
-    };
-  }
-  showSettings (event) {
-    event.preventDefault();
-  }
-  compnentDidMount(){
-    this.handleWindowChange();
-    window.addEventListener("resize",this.handleWindowChange());
-  }
-  compnentDidUnMount(){
-    window.removeEventListener("resize",this.handleWindowChange());
-  }
-  handleWindowChange(){
-    this.setState({
-      width: window.innerWidth,
-    });
-  }
-
   render(){
-    const isMobile = this.state.width <= 760;
-    if (isMobile){
       return(
-
+        <div>
+        <div className="navbar-dev">
           <Menu>
             <li><Link to="/">Hem</Link></li>
             <li><Link to="./presentation">Presentation</Link></li>
@@ -40,11 +16,8 @@ export class Navbar extends React.Component{
             <li><Link to="./priser">Priser</Link></li>
             <li><Link to="./kontakt">Bokning/Kontakt</Link></li>
           </Menu>
-      )
-    }
-    else{
-      return (
-        <nav>
+        </div>
+        <nav className="navbar-web">
           <ul className="navbar">
             <li><Link to="/" className={"sbtn"}>Hem</Link></li>
             <li><Link to="./presentation">Presentation</Link></li>
@@ -53,7 +26,7 @@ export class Navbar extends React.Component{
             <li><Link to="./kontakt">Bokning/Kontakt</Link></li>
           </ul>
         </nav>
+        </div>
       )
-    }
   }
 }
